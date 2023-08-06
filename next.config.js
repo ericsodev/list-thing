7e7b41dd4f32270aa37442486a3e3c391ab2d844
Eric <eric.so.dev@gmail.com>
2023-08-06 14:22:30 -0400

    redirect casing

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects: async () => [
+    {
+      source: "/list",
+      destination: "/list/new",
+      permanent: true,
+    },
+  ],
+  images: {
+    domains: ["ui-avatars.com"],
+  },
 };
 
 module.exports = nextConfig;
