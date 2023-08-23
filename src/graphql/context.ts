bf71289b96f21c36ea49ed8701a3bf2c9c32847c
Eric <eric.so.dev@gmail.com>
2023-08-23 19:10:16 -0400

    added migration push

--- src/graphql/context.ts
+++ src/graphql/context.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { db } from "@prisma/client";
 import prisma from "../../lib/prisma";
 import { NextApiRequest, NextApiResponse } from "next";
 import jwt from "jsonwebtoken";
