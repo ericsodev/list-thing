ca9c8d6216999fccf57be8387299c8311eb5ed6a
Eric <eric.so.dev@gmail.com>
2023-08-23 19:10:16 -0400

    added migration push

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,12 +1,8 @@
-  import { drizzle, PostgresJsDatabase } from 'drizzle-orm/postgres-js';
-import { migrate } from 'drizzle-orm/postgres-js/migrator';
-import postgres from 'postgres';
- 
-// for migrations
-const migrationClient = postgres("postgres://postgres:adminadmin@0.0.0.0:5432/db", { max: 1 });
-migrate(drizzle(migrationClient), {migrationsFolder})
- 
+import { env } from "@/env";
+import { drizzle, PostgresJsDatabase } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import {} from "./migrate";
+
 // for query purposes
-const queryClient = postgres("postgres://postgres:adminadmin@0.0.0.0:5432/db");
-const db: PostgresJsDatabase = drizzle(queryClient);
-await db.select().from(...)...
+const queryClient = postgres(env.DATABASE_URL);
+export const db: PostgresJsDatabase = drizzle(queryClient);
