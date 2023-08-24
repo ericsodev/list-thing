528922400a6c23ac26bf831ca304001bf148fe27
Eric <eric.so.dev@gmail.com>
2023-08-24 16:43:36 -0400

    changed db driver and ssl

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,8 +1,13 @@
 import { env } from "@/env";
 import { drizzle, PostgresJsDatabase } from "drizzle-orm/postgres-js";
+import { readFileSync } from "fs";
 import postgres from "postgres";
-import {} from "./migrate";
 
 // for query purposes
-const queryClient = postgres(env.DATABASE_URL);
+
+const queryClient = postgres(env.DATABASE_URL, {
+  ssl: {
+    ca: readFileSync(env.DATABASE_SSL_PATH).toString(),
+  },
+});
 export const db: PostgresJsDatabase = drizzle(queryClient);
