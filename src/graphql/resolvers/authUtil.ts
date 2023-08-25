110afe454971a16b6cf5cf3e766801ecee0e20c0
Eric <eric.so.dev@gmail.com>
2023-08-24 20:25:23 -0400

    updated auth resolver wrapper for drizzle

--- src/graphql/resolvers/authUtil.ts
+++ src/graphql/resolvers/authUtil.ts
@@ -3,6 +3,7 @@ import { User } from "../types/graphql";
 import { verifyAccessToken, verifyRefreshToken } from "../util/token";
 import { Context } from "../context";
 import CUSTOM_ERRORS from "../errorCodes";
+import { user } from "@/db/schema";
 
 /**
  *  Check for valid refresh tokens and return user data from token as payload,
@@ -52,27 +53,19 @@ export function authorized<InputType>(
     }
 
     // validate access token
-    const user = await verifyAccessToken(ctx.req.headers.authorization);
-    if (!user) {
+    const tokenUser = await verifyAccessToken(ctx.req.headers.authorization);
+    if (!tokenUser) {
       throw new GraphQLError(...CUSTOM_ERRORS.INVALID_TOKEN);
     }
 
     // check if user exists in db
-    const userDb = await ctx.prisma.user.findUnique({
-      where: {
-        id: user.id,
-      },
-      select: {
-        id: true,
-        name: true,
-      },
-    });
+    const userSelect = await ctx.db.select({ id: user.id }).from(user);
 
-    if (!userDb) {
+    if (!userSelect[0]) {
       throw new GraphQLError(...CUSTOM_ERRORS.NO_USER);
     }
 
-    ctx.user = user;
+    ctx.user = tokenUser;
     return await fn(parent, args, ctx, info);
   };
 }
