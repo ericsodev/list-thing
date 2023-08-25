3f92ca2ee13d27b96bc0ee30b10b112350323073
Eric <eric.so.dev@gmail.com>
2023-08-25 01:00:10 -0400

    updated user resolver for drizzle

--- src/graphql/resolvers/UserResolver.ts
+++ src/graphql/resolvers/UserResolver.ts
@@ -1,27 +1,55 @@
+import { user } from "@/db";
 import { Context } from "../context";
 import { QueryUsersArgs, User } from "../types/graphql";
 import { authorized, hasValidToken } from "./authUtil";
+import { AnyColumn, SQL, and, eq, like, or } from "drizzle-orm";
+
+function opt<T>(v: unknown, sql: SQL<T>): SQL<T> | undefined {
+  return v === undefined || v === null ? undefined : sql;
+}
 
 const resolver = {
   Query: {
     self: authorized(async (_p, _a, ctx, _i) => {
       return ctx.user;
     }),
-    users: async (_parent: any, args: QueryUsersArgs, ctx: Context): Promise<User[]> => {
-      return await ctx.prisma.user.findMany({
-        where: {
-          id: args.input.id ?? undefined,
-          name: {
-            equals: args.input.name ?? undefined,
-            startsWith: args.input.filter?.startsWith ?? undefined,
-            contains: args.input.filter?.contains ?? undefined,
-          },
-        },
-        select: {
-          name: true,
-          id: true,
-        },
-      });
+    users: async (
+      _parent: any,
+      { input: { name, id } }: QueryUsersArgs,
+      ctx: Context,
+    ): Promise<User[]> => {
+      return await ctx.db
+        .select({ id: user.id, name: user.name })
+        .from(user)
+        .where(
+          and(
+            opt(id, eq(user.id, id!)),
+            or(
+              opt(name?.equals, eq(user.name, name?.equals!)),
+              opt(name?.startsWith, like(user.name, `${name?.startsWith!}%`)),
+              opt(name?.contains, like(user.name, `%${name?.contains!}%`)),
+            ),
+          ),
+        )
+        .limit(30);
+      //   return await ctx.db
+      //     .select({ id: user.id, name: user.name })
+      //     .from(user)
+      //     .where(or(eq(user.name, input.filter?.equals || ""), like("")));
+      // return await ctx.prisma.user.findMany({
+      //   where: {
+      //     id: args.input.id ?? undefined,
+      //     name: {
+      //       equals: args.input.name ?? undefined,
+      //       startsWith: args.input.filter?.startsWith ?? undefined,
+      //       contains: args.input.filter?.contains ?? undefined,
+      //     },
+      //   },
+      //   select: {
+      //     name: true,
+      //     id: true,
+      //   },
+      // });
     },
   },
 };
