7075fdb6626d7223c2a41332bd559889ea279be0
Eric <eric.so.dev@gmail.com>
2023-08-25 01:02:01 -0400

    updated tag resolver for drizzle

--- src/graphql/resolvers/TagResolver.ts
+++ src/graphql/resolvers/TagResolver.ts
@@ -1,17 +1,16 @@
+import { tag } from "@/db";
 import { Context } from "../context";
 import { TagSearchInput } from "../types/graphql";
 import { authorized } from "./authUtil";
+import { eq } from "drizzle-orm";
 
 const tagResolver = {
   Query: {
     tagSearch: authorized(async (_p, args: TagSearchInput, ctx: Context) => {
-      const result = await ctx.prisma.tag.findMany({
-        where: {
-            listId: args.listId,
-        
-        },
-        take: 10,
-      });
+      return await ctx.db
+        .select({ id: tag.id, name: tag.name })
+        .from(tag)
+        .where(eq(tag.listId, args.listId));
     }),
   },
 };
