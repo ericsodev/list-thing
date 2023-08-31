55842813d88aab956ffbbd255b09f3535a4ab930
Eric <eric.so.dev@gmail.com>
2023-08-31 18:17:13 -0400

    added cb as argument for setState

--- src/components/listPage/CommandPalette/CommandContext.tsx
+++ src/components/listPage/CommandPalette/CommandContext.tsx
@@ -3,42 +3,46 @@ import { Suggestion } from "./Suggestions";
 
 export type Mode = "normal" | "search" | "delete" | "create";
 type CommandCtx = [
-  {
-    mode: Mode;
-    input: string;
-    suggestions: Suggestion[];
-    isOpen: boolean;
-    selectedSuggestion?: number;
-  },
-  (value: Partial<CommandCtx[0]>) => void,
+    {
+        mode: Mode;
+        input: string;
+        suggestions: Suggestion[];
+        isOpen: boolean;
+        selectedSuggestion?: number;
+    },
+    (value: Partial<CommandCtx[0]> | ((curr: CommandCtx[0]) => CommandCtx[0])) => void,
 ];
 
 const commandCtx = React.createContext<CommandCtx>([
-  {
-    mode: "normal",
-    suggestions: [],
-    input: "",
-    isOpen: false,
-  },
-  () => {},
+    {
+        mode: "normal",
+        suggestions: [],
+        input: "",
+        isOpen: false,
+    },
+    () => {},
 ]);
 
 type Props = {} & React.PropsWithChildren;
 export function CommandProvider({ children }: Props) {
-  const [value, setValue] = React.useState<CommandCtx[0]>({
-    mode: "normal",
-    suggestions: [],
-    input: "",
-    isOpen: false,
-  });
-  const setVal = useCallback(
-    (v: Partial<CommandCtx[0]>) => {
-      setValue((old) => ({ ...old, ...v }));
-    },
-    [setValue],
-  );
-  return <commandCtx.Provider value={[value, setVal]}>{children}</commandCtx.Provider>;
+    const [value, setValue] = React.useState<CommandCtx[0]>({
+        mode: "normal",
+        suggestions: [],
+        input: "",
+        isOpen: false,
+    });
+    const setVal = useCallback(
+        (v: Partial<CommandCtx[0]> | ((curr: CommandCtx[0]) => CommandCtx[0])) => {
+            if (typeof v === "function") {
+                setValue(v);
+                return;
+            }
+            setValue((old) => ({ ...old, ...v }));
+        },
+        [setValue],
+    );
+    return <commandCtx.Provider value={[value, setVal]}>{children}</commandCtx.Provider>;
 }
 export function useCommand() {
-  return React.useContext(commandCtx);
+    return React.useContext(commandCtx);
 }
